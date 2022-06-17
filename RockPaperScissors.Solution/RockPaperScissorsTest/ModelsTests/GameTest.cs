789e6b2dbb05561953ed9a98b12de6c647fbd663
Lucian Miller <lucian.miller185@gmail.com>
2022-06-17 10:41:29 -0700

    Add failing test for Game object

--- RockPaperScissors.Solution/RockPaperScissorsTest/ModelsTests/GameTest.cs
+++ RockPaperScissors.Solution/RockPaperScissorsTest/ModelsTests/GameTest.cs
@@ -6,11 +6,21 @@ using RockPaperScissors.Models;
 namespace RockPaperScissors.Tests
 {
   [TestClass]
-  public class GameTests: IDisposable
+  public class GameTests
+  //use line below when its time to start clearing data between tests
+  //public class GameTests: IDisposable
+  //lesson ref (3.1.2.6)
   {
-    public void Dispose()
+  //   public void Dispose()
+  //   {
+  //     Game.ClearAll();
+  //   }
+
+    [TestMethod]
+    public void GameConstructor_CreateInstanceOfGame_Game()
     {
-      Game.ClearAll();
+      Game newGame = new Game();
+      Assert.AreEqual(typeof(Game), newGame.GetType());
     }
   }
 }
