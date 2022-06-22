8df5187f8186d2152c41c846913a16c0ab6b1f86
Lucian Miller <lucian.miller185@gmail.com>
2022-06-22 11:04:46 -0700

    Added ability to change turns value

--- RockPaperScissors.Solution/RockPaperScissors/Models/Game.cs
+++ RockPaperScissors.Solution/RockPaperScissors/Models/Game.cs
@@ -2,13 +2,18 @@ namespace RockPaperScissors.Models
 {
   public class Game
   {
-    public string Player1Turn;
-    public string Player2Turn;
+    public string Player1Turn { get; set; }
+    public string Player2Turn { get; set; }
 
+    public Game(string playerOneTurn, string playerTwoTurn)
+    {
+      Player1Turn = playerOneTurn;
+      Player2Turn = playerTwoTurn;
 
     // public static void ClearAll()
     // {
     //   _instances.Clear();
     // }
+    }
   }
 }
