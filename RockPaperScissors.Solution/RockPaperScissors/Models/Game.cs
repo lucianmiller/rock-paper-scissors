4c068b335584bdc02024a6b8566b93d31fd3f09f
Lucian Miller <lucian.miller185@gmail.com>
2022-08-16 11:26:24 -0700

    Add ability to start game and computer turn random

--- RockPaperScissors.Solution/RockPaperScissors/Models/Game.cs
+++ RockPaperScissors.Solution/RockPaperScissors/Models/Game.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace RockPaperScissors.Models
 {
   public class Game
@@ -5,15 +7,48 @@ namespace RockPaperScissors.Models
     public string Player1Name { get; set; }
     public string Player2Name { get; set; }
 
-    // public static void ClearAll()
-    // {
-    //   _instances.Clear();
-    // }
-    
     public Game(string player1Name, string player2Name)
     {
       Player1Name = player1Name;
       Player2Name = player2Name;
     }
+    // public static void ClearAll()
+    // {
+    //   _instances.Clear();
+    // }
+
+    public string ComputerTurn() 
+    {
+      string[] options = {"rock", "paper", "scissors"};
+      Random rand = new Random();
+      int randNum = rand.Next(0, 3);
+      return options[randNum];
+    }
+  }
+
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Welcome to Rock, Paper, Scissors!");
+      Console.WriteLine("Please enter your name:");
+      string playerOne = Console.ReadLine();
+
+      Game newGame = new Game(playerOne, "Computer");
+      string continueGame = "y";
+
+      while (continueGame == "y")
+      {
+        Console.WriteLine("Would you like to play a round? (y or n)");
+        continueGame = Console.ReadLine();
+
+        Console.WriteLine(newGame.ComputerTurn());
+      }
+      Console.WriteLine("Game Over!");
+    }
   }
 }
+// next steps
+// 2) prompt user for rock paper or scissors and store in variable
+// 3) method determining win or lose
+// create player and computer score properties in class and constructor
